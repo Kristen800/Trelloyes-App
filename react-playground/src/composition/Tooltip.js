ddd86d429a0f69a83b0be4c79b31eb514ba3b8e6
Kristen Riley <kriley1983@hotmail.com>
2021-01-21 09:57:56 -0700

    deleted HelloWorld added tooltip added trelloyes

--- react-playground/src/composition/Tooltip.js
+++ react-playground/src/composition/Tooltip.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Tooltip.css';
 
-function Tooltip(props) {
+/*function Tooltip(props) {
     return (
         <span className='Tooltip'>
             <span 
@@ -15,12 +15,29 @@ function Tooltip(props) {
             </div>
         </span>
     );
-}
+}*/
 
 class TooltipClass extends React.Component {
+    static defaultProps = {
+        color: '#01A800',
+    };
     render() {
-        return <span />
+        console.log('Using a class componenet!')
+        console.log(this.props)
+        return ( 
+            <span className='Tooltip'>
+            <span
+                className='Tooltip-content'
+                style={{ color:this.props.color}}
+            >
+                {this.props.children}
+            </span>
+            <div className='Tooltip-message'>
+                {this.props.message}
+            </div>
+        </span>
+        )
     }
 }
 
-export default Tooltip;
+export default TooltipClass;
